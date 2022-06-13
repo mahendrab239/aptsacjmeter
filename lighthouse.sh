2bd192bc8d4dc52488590b6b8b1f8540601dfff7
mahendrab239 <106797893+mahendrab239@users.noreply.github.com>
2022-06-13 19:16:58 +0530

    Update lighthouse.sh

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
 python2 LightHouse_1_1.py -i URL.csv -b ${BUILD_NUMBER}
-cat lighthouse.csv >> /opt/aptsac/jmeter/workspace/report/lighthouse.csv
-echo $WORKSPACE
-mv lighthouse.csv ../results/lighthouse_${BUILD_NUMBER}.csv
