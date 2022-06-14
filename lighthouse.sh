5dafb5e1bf5c28fc092f7f6f2ae638b41ecea9c3
mahendrab239 <106797893+mahendrab239@users.noreply.github.com>
2022-06-14 11:43:18 +0530

    Update lighthouse.sh

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 python2 LightHouse_1_1.py -i URL.csv -b ${BUILD_NUMBER}
-docker cp /report/lighthouse.csv /opt/aptsac/jmeter/workspace/report
+cat /report/lighthouse.csv >> /usr/tmp/lighthouse.csv
+mv /report/lighthouse.csv /usr/tmp/lighthouse_${BUILD_NUMBER}.csv
