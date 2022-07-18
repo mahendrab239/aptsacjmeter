7a52a1387465d8021cbdedd33df3b120fc08c5ad
mahendrab239 <106797893+mahendrab239@users.noreply.github.com>
2022-07-18 15:27:29 +0530

    Update lighthouse.sh

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python2 LightHouse_1_1.py -i URL.csv -b ${BUILD_NUMBER} -c ${Config}
 cp /report/lighthouse.csv /usr/tmp/lighthouse.csv
-cp /report/lighthouse.csv /usr/tmp/lighthouse_${BUILD_NUMBER}.csv
+mv /report/lighthouse.csv /usr/tmp/lighthouse_${BUILD_NUMBER}.csv
